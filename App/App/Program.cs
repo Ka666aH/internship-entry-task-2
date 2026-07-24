8f8272a536a9b733a46915ab747f9ae82e872612
Ka6aH <dmitryhoroshev05@mail.ru>
2026-07-24 11:29:47 +0300

    Настроить конвертер для перечислений.

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,9 +1,16 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseUpper));
+});
 var app = builder.Build();
 
 app.MapControllers();
