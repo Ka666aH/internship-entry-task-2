f0c07eb66ace4ed87311a66c3f37f4dcd94c3c3d
Ka6aH <dmitryhoroshev05@mail.ru>
2026-07-24 11:27:01 +0300

    Реализовать сущность операции.

---
+++ App/App/Domain/Enums/OperationStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace App.Domain.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum OperationStatus
+    {
+        Created,
+        Processing,
+        Completed,
+        Rejected
+    }
+}
---
+++ App/App/Domain/Operation.cs
@@ -0,0 +1,31 @@
+﻿using App.Domain.Enums;
+
+namespace App.Domain
+{
+    public class Operation
+    {
+        public string OperationId { get; init; }
+        public decimal Amount { get; init; }
+        public string Currency { get; init; }
+        public string? Description { get; init; }
+        public OperationStatus Status { get; private set; } = OperationStatus.Created;
+        public Guid? ProviderPaymentId { get; private set; } = null;
+        public Operation(string operationId ,decimal amount, string currency, string? description)
+        {
+            OperationId = operationId;
+            Amount = amount;
+            Currency = currency;
+            Description = description;
+        }
+        public void Submit() => Status = OperationStatus.Processing;
+        public void Complete() => Status = OperationStatus.Completed;
+        public void Reject() => Status = OperationStatus.Rejected;
+        public void SetProviderPaymentId(Guid providerPaymentId)
+        {
+            if (ProviderPaymentId is not null) 
+                throw new InvalidOperationException("ProviderPaymentId уже записан.");
+            ProviderPaymentId = providerPaymentId;
+        }
+
+    }
+}
